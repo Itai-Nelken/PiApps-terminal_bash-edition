5b0a27909dab372f7dd6aada3aa419f3483eeeb8
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-03-23 11:19:28 +0200

    Update pi-apps-terminal-bash-edition.sh

--- pi-apps-terminal-bash-edition.sh
+++ pi-apps-terminal-bash-edition.sh
@@ -62,15 +62,16 @@ function search() {
         dirname=$(basename "$dir")
         if [[ "$dirname" != "template" ]]; then
             #echo $dirname
-		    if [[ $dirname == *$1* ]]; then
+		    if [[ $dirname == "*$1*" ]]; then
 				#echo "FIRST"
 				echo -e "${bold}${inverted}${light_blue}$dirname${normal}"
 				DESC="$(cat $PI_APPS_DIR/apps/$dirname/description)"
                 echo -e "${green}$DESC${normal}"
 	    	elif grep -q "$1" "$PI_APPS_DIR/apps/$dirname/description" ; then
 				#echo "SECOND"
-				echo $dirname
-				cat "$PI_APPS_DIR/apps/$dirname/description"
+                echo -e "${bold}${inverted}${light_blue}$dirname${normal}"
+				DESC="$(cat $PI_APPS_DIR/apps/$dirname/description)"
+                echo -e "${green}$DESC${normal}"
    	    	fi
         fi
     done
